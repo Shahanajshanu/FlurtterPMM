9dc4c4e42c33bd4bdfa79089a4b5b8006747e3a0
Shahanajshanu <mhmmdshahanaj3123@gmail.com>
2022-04-14 08:52:43 +0530

    2nd day

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pmm/models/category/category_model.dart';
+//import 'package:hive/hive.dart';
 import 'package:pmm/screens/home/screen_home.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  if(!Hive.isAdapterRegistered(CategoryTypeAdapter().typeId)) {
+    Hive.registerAdapter(CategoryTypeAdapter());
+  }
+
+  if(!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
+
   runApp(const MyApp());
 }
 
@@ -29,4 +43,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
